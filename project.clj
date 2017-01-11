32e50d403414cc3b660d0975bbd8b053b0f46ab1
ronen <narkisr@gmail.com>
2017-01-11 16:26:04 +1100

    using simpleton

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject clojure-intro "0.0.9"
+
+  :description "A clojure intro talk "
+
+  :url "https://github.com/narkisr/self-build"
+
+  :license  {
+    :name "Licensed under a Creative Commons Attribution 3.0 Unported License." :url "http://creativecommons.org/licenses/by/3.0/"
+  }
+
+  :dependencies [[org.clojure/clojure "1.8.0"] ]
+
+  :plugins [[lein-simpleton "1.3.0"]]
+
+)
